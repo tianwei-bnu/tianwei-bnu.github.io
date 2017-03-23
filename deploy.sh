f2fc866449e0590aef3fd0a07fa0bd93397adea4
whiskytina <weitian.bnu@gmail.com>
2017-03-23 21:10:10 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+git remote add github https://github.com/whiskytina/whiskytina.github.io.git
+git remote add gitcafe https://git.coding.net/whiskytina/whiskytina.git
 
 git add .
 git commit -m "update"
-git push -u origin master
+
+git push -u github master
+git push -u gitcafe master
